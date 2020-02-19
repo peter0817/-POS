e1f0ae8cbdc74d229a7e46a7fe2a10e591f66e3e
peter0817 <59242083+peter0817@users.noreply.github.com>
2020-02-19 00:57:33 +0000

    Create AuthResultObject.cs

---
+++ AuthResultObject.cs
@@ -0,0 +1,32 @@
+internal class AuthResultObject
+{
+	public string inUse
+	{
+		get;
+		set;
+	}
+
+	public string errorCode
+	{
+		get;
+		set;
+	}
+
+	public string message
+	{
+		get;
+		set;
+	}
+
+	public string serial
+	{
+		get;
+		set;
+	}
+
+	public string shopType
+	{
+		get;
+		set;
+	}
+}
