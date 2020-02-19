a7b53123f45f71475b1db5f78d874da895df2bdd
peter0817 <59242083+peter0817@users.noreply.github.com>
2020-02-19 00:58:15 +0000

    Create ComboboxItem.cs

---
+++ ComboboxItem.cs
@@ -0,0 +1,25 @@
+public class ComboboxItem
+{
+	public string Text
+	{
+		get;
+		set;
+	}
+
+	public object Value
+	{
+		get;
+		set;
+	}
+
+	public ComboboxItem(string text, object value)
+	{
+		Text = text;
+		Value = value;
+	}
+
+	public override string ToString()
+	{
+		return Text;
+	}
+}
